eb19038c48e000fa30d6efca463c72bee0ec5b22
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-21 15:13:09 +0000

    created _form partial for new and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: %i[show update delete]
+  before_action :find_article, only: %i[show edit update delete]
 
   def index
     @articles = Article.all
@@ -9,15 +9,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def create
+    article = Article.create(article_params)
+    redirect_to article_path(article)
   end
 
   def edit
-
   end
+
   def update
-
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
+
   def delete
 
   end
@@ -28,7 +35,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def article_parms
-
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
