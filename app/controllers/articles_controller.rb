59fe2c4b117f817b028ca372f0a56345a3a83819
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-21 14:53:21 +0000

    created all the necessary actions in the controller + show and index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,34 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: %i[show update delete]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+  def update
+
+  end
+  def delete
+
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_parms
+
+  end
 end
